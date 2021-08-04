07fb0f30fd4b3d3efd89b21dad78969f99c0331b
Phạm Phương Nam <phuongnamp99@gmail.com>
2021-08-04 21:08:01 +0700

    fix h10 error in heroku

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const Express  = require('express')
+const Express = require('express')
 const Morgan = require('morgan')
 const helmet = require('helmet')
 const rotatingFileStream = require('rotating-file-stream')
@@ -9,7 +9,6 @@ const scheduleRouter = require('./router/ScheduleRouterAPIs')
 const bodyParser = require('body-parser')
 
 const isProduction = process.env.NODE_ENV === 'production'
-const PORT = process.env.PORT || 4000
 const app = Express()
 
 // set up lib
@@ -24,20 +23,24 @@ app.use(isProduction ? Morgan('combined', {
 app.use(Cors())
 
 app.use(Express.json())
-app.use(Express.urlencoded({ extended: false }))
+app.use(Express.urlencoded({
+    extended: false
+}))
 app.use(Express.static(path.join(__dirname, 'public')))
 
 // su dung bo phan giai application/json
 app.use(bodyParser.json());
 
 // su dung bo phan giai application/xwww-
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 //form-urlencoded
 app.use(Express.static('/'))
 
 app.use('/user', userRouter)
 app.use('/schedule', scheduleRouter)
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log(`Server is listening on port: ${PORT}`)
 })
