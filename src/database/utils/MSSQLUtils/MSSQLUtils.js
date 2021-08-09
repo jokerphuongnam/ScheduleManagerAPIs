987561b986a4a5d68713a4315dfc7311ad09207f
Phạm Phương Nam <phuongnamp99@gmail.com>
2021-08-09 20:29:00 +0700

    mssql utils if type is bigint and not null will convert to Number

--- src/database/utils/MSSQLUtils/MSSQLUtils.js
+++ src/database/utils/MSSQLUtils/MSSQLUtils.js
@@ -2,16 +2,16 @@ const {
     Request,
     Connection
 } = require('tedious')
-const configure = require('./configure.json') 
+const configure = require('./configure.json')
 
 function MSSQLUtils(configureParam) {
     const config = configureParam ? configureParam : configure
 
     return new class {
         getRequest(sqlQuery, callback = () => {}) {
-            return new Request(sqlQuery, callback);
+            return new Request(sqlQuery, callback)
         }
-    
+
         open() {
             return new Promise((resolve, reject) => {
                 const conn = new Connection(config)
@@ -26,27 +26,31 @@ function MSSQLUtils(configureParam) {
                 })
             })
         }
-    
+
         execute(sqlQuery) {
             return new Promise(async (resolve, reject) => {
                 const conn = await this.open();
                 let error = undefined
                 const request = this.getRequest(sqlQuery, (err) => {
                     error = err
-                });
+                })
                 const array = []
                 request.on("row", (columns) => {
                     const data = {}
                     columns.forEach((column) => {
-                        data[column.metadata.colName] = column.value;
-                    });
+                        let value = column.value
+                        if(value && column.metadata.type.type.toLowerCase() == 'intn') {
+                            value = Number(value)
+                        }
+                        data[column.metadata.colName] = value
+                    })
                     array.push(data)
-                });
+                })
                 request.on("error", (err) => {
                     conn.close()
                     reject(err)
                     error = err
-                });
+                })
                 request.on("done", function (rowCount, more) {});
                 request.on("requestCompleted", function () {
                     if (error) {
@@ -55,9 +59,9 @@ function MSSQLUtils(configureParam) {
                         resolve(array)
                     }
                     conn.close()
-                });
+                })
                 conn.execSql(request)
-            });
+            })
         }
     }()
 }
